fa9d7d8d5c5f2fc34bdffe24f2b532f3a3fef650
isminuraen <isminuraen@gmail.com>
2016-09-27 17:20:15 +0700

    Add files via upload

---
+++ arduinoismi.ino.ino
@@ -0,0 +1,13 @@
+//the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin 3 as an output
+  pinMode(3, OUTPUT);
+}
+
+//the loop function runs over and over again forever
+void loop() {
+  digitalWrite(3, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(4000);            // wait for a second
+  digitalWrite(3, LOW);   // turn the lED off by making the voltage Low
+  delay(4000);            // wait for a second
+}
